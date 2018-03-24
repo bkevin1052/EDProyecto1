b7144440883ab36f8b926e6f945037850fb0dafc
Kevin Estuardo Barrientos Calderon <bkevin1052@gmail.com>
2018-03-24 13:10:17 -0600

    Agregando Proyecto1

---
+++ LibreriaDeClases/Clases/Nodo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaDeClases.Clases
+{
+    public class Nodo<T>
+    {
+        public T[] valores;
+        public Nodo<T>[] nodo;
+        public static int numValores;
+        public bool tengoHijos = false;
+        public int ocupados = 0;
+        public Nodo<T> padre;
+        public Nodo()
+        {
+            //nodo = new Nodo[Raiz.grado * 2 + 3];
+            //valores = new int[Raiz.grado * 2 + 1];
+        }
+    }
+}
