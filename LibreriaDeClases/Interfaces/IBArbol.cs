d609769fed7278585e4dfc8268e7d2d8637d6400
Kevin Estuardo Barrientos Calderon <bkevin1052@gmail.com>
2018-04-04 16:47:04 -0600

    Agregando Interfaces

---
+++ LibreriaDeClases/Interfaces/IBArbol.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibreriaDeClases.Clases;
+
+namespace LibreriaDeClases.Interfaces
+{
+    public interface IBArbol<TK,TP>
+    {
+        Entry<TK, TP> Search(TK key);
+        void Insertar(TK nuevaLlave, TP nuevoApuntador);
+
+    }
+}
