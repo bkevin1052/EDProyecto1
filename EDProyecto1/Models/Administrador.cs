31c8e6554206e31a919660f17cea453e01620262
Kevin Estuardo Barrientos Calderon <bkevin1052@gmail.com>
2018-04-14 14:53:46 -0600

    Agregando vistas

---
+++ EDProyecto1/Models/Administrador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EDProyecto1.Models
+{
+    public class Administrador
+    {
+
+        public string Nombre { get; set; }
+        public string Password { get; set; }
+    }
+}
