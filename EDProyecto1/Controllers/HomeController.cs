11c1797ba82ca929fa8d0ac763f3347ed5f60bf3
Kevin Estuardo Barrientos Calderon <bkevin1052@gmail.com>
2018-04-14 15:22:42 -0600

    Agregando vistas (login)

--- EDProyecto1/Controllers/HomeController.cs
+++ EDProyecto1/Controllers/HomeController.cs
@@ -16,15 +16,11 @@ namespace EDProyecto1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
