4673ff69c5320aa8e9c98ae93138eeee543fbdef
LeslyDubon <leslykarinadubon@yahoo.com>
2018-04-16 22:29:13 -0600

    Creación de los árboles e ingreso de grado

--- EDProyecto1/Controllers/HomeController.cs
+++ EDProyecto1/Controllers/HomeController.cs
@@ -4,14 +4,35 @@ using System.Linq;
 using System.Web;
 using LibreriaDeClases.Clases;
 using System.Web.Mvc;
+using EDProyecto1.DBContext;
+using EDProyecto1.Models;
+using System.IO;
 
 namespace EDProyecto1.Controllers
 {
     public class HomeController : Controller
     {
+        string rutaUsuarios = @"C:\Users\" + Environment.UserName + @"\users.tree";
+        string rutaArbolShowporNombre = @"C:\Users\" + Environment.UserName + @"\name.showtree";
+        string rutaArbolShowporGenero = @"C:\Users\" + Environment.UserName + @"\genre.showtree";
+        string rutaArbolShowporAnio = @"C:\Users\" + Environment.UserName + @"\year.showtree";
+        string rutaArbolMovieporNombre = @"C:\Users\" + Environment.UserName + @"\name.movietree";
+        string rutaArbolMovieporGenero = @"C:\Users\" + Environment.UserName + @"\genre.movietree";
+        string rutaArbolMovieporAnio = @"C:\Users\" + Environment.UserName + @"\year.movietree";
+        string rutaArbolDocumentaryporNombre = @"C:\Users\" + Environment.UserName + @"\name.documentarytree";
+        string rutaArbolDocumentaryporGenero = @"C:\Users\" + Environment.UserName + @"\genre.documentarytree";
+        string rutaArbolDocumentaryporAnio = @"C:\Users\" + Environment.UserName + @"\year.documentarytree";
         public ActionResult Index()
         {
-            return View();
+            if(!System.IO.File.Exists(rutaUsuarios))
+            {
+                return View("IngresoGrado");
+            }
+            else
+            {
+                return View();
+            }
+            
         }
 
         public ActionResult About()
@@ -22,6 +43,60 @@ namespace EDProyecto1.Controllers
         public ActionResult Contact()
         {
             return View();
+        }
+        public ActionResult IngresoGrado()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult IngresoGrado(string Grado)
+        {
+            int grado;
+            if(int.TryParse(Grado, out grado))
+            {
+                grado = int.Parse(Grado);
+                if (grado > 1)
+                {
+                    
+                    EscribirGrado(rutaUsuarios, false, grado);
+                    DefaultConnection.BArbolShowPorNombre = new BArbol<string, Audiovisual>(grado);
+                    EscribirGrado(rutaArbolShowporNombre, false, grado);
+                    DefaultConnection.BArbolShowPorAnio = new BArbol<int, Audiovisual>(grado);
+                    EscribirGrado(rutaArbolShowporAnio, false, grado);
+                    DefaultConnection.BArbolShowPorGenero = new BArbol<string, Audiovisual>(grado);
+                    EscribirGrado(rutaArbolShowporGenero, false, grado);
+                    DefaultConnection.BArbolMoviePorNombre = new BArbol<string, Audiovisual>(grado);
+                    EscribirGrado(rutaArbolMovieporNombre, false, grado);
+                    DefaultConnection.BArbolMoviePorAnio = new BArbol<int, Audiovisual>(grado);
+                    EscribirGrado(rutaArbolMovieporAnio, false, grado);
+                    DefaultConnection.BArbolMoviePorGenero = new BArbol<string, Audiovisual>(grado);
+                    EscribirGrado(rutaArbolMovieporGenero, false, grado);
+                    DefaultConnection.BArbolDocumentaryPorNombre = new BArbol<string, Audiovisual>(grado);
+                    EscribirGrado(rutaArbolDocumentaryporNombre, false, grado);
+                    DefaultConnection.BArbolDocumentaryPorAnio = new BArbol<int, Audiovisual>(grado);
+                    EscribirGrado(rutaArbolDocumentaryporAnio, false, grado);
+                    DefaultConnection.BArbolDocumentaryPorGenero = new BArbol<string, Audiovisual>(grado);
+                    EscribirGrado(rutaArbolDocumentaryporGenero, false, grado);
+                    return View("Index");
+                }
+                else
+                {
+                    TempData["alertMessage"] = "Ingrese un grado válido. El grado debe ser mayor o igual a 2.";
+                    return View();
+                }
+            }
+            else
+            {
+                TempData["alertMessage"] = "Ingrese un grado válido. El grado debe ser mayor o igual a 2.";
+                return View();
+            }
+            return View();
+        }
+        public void EscribirGrado(string rutaArchivo, bool sobrescribir, int grado)
+        {
+            StreamWriter GradoWriter = new StreamWriter(rutaArchivo, sobrescribir);
+            GradoWriter.WriteLine("Grado: "+grado.ToString());
+            GradoWriter.Close();
         }
     }
 }
--- EDProyecto1/DBContext/DefaultConnection.cs
+++ EDProyecto1/DBContext/DefaultConnection.cs
@@ -13,9 +13,15 @@ namespace EDProyecto1.DBContext
         private static volatile DefaultConnection Instance;
         private static object syncRoot = new Object();
 
-        public static BArbol<string, Audiovisual> BArbolPorNombre = new BArbol<string, Audiovisual>(3);
-        public static BArbol<int, Audiovisual> BArbolPorAnio = new BArbol<int, Audiovisual>(3);
-        public static BArbol<string, Audiovisual> BArbolPorGenero = new BArbol<string, Audiovisual>(3);
+        public static BArbol<string, Audiovisual> BArbolShowPorNombre;
+        public static BArbol<int, Audiovisual> BArbolShowPorAnio = new BArbol<int, Audiovisual>(3);
+        public static BArbol<string, Audiovisual> BArbolShowPorGenero = new BArbol<string, Audiovisual>(3);
+        public static BArbol<string, Audiovisual> BArbolMoviePorNombre = new BArbol<string, Audiovisual>(3);
+        public static BArbol<int, Audiovisual> BArbolMoviePorAnio = new BArbol<int, Audiovisual>(3);
+        public static BArbol<string, Audiovisual> BArbolMoviePorGenero = new BArbol<string, Audiovisual>(3);
+        public static BArbol<string, Audiovisual> BArbolDocumentaryPorNombre = new BArbol<string, Audiovisual>(3);
+        public static BArbol<int, Audiovisual> BArbolDocumentaryPorAnio = new BArbol<int, Audiovisual>(3);
+        public static BArbol<string, Audiovisual> BArbolDocumentaryPorGenero = new BArbol<string, Audiovisual>(3);
         public static List<Usuario> usuarios = new List<Usuario>();
 
         public int IDActual { get; set; }
